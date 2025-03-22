a31d86fdf9fece71b607ffb9878e6b76c18ebf5e
chkg2a <xshalltearvamps@gmail.com>
2025-03-22 23:44:31 +0530

    add constant about

---
+++ src/constants/About.js
@@ -0,0 +1,25 @@
+export const about = [
+  {
+    title: "TEDx",
+    subtitle: "NERIST",
+    description:
+      "Lighthouse Apus embodies a radiant beacon, illuminating pathways toward imaginative discovery and guiding explorers through uncharted realms with clarity and purpose. ",
+    subdescription: "",
+  },
+  {
+    title: "TED",
+    subtitle: "",
+    description:
+      "TED (Technology, Entertainment, and Design) began with topics like e-books and fractals but has evolved into a global platform for creativity and innovation. It now features a diverse range of voices, including business leaders, scientists, and philosophers.",
+    subdescription:
+      "TED’s initiatives, such as TED-Ed for education, TEDGlobal conferences, and the TED Prize for impactful change, continue to inspire inclusivity, progress, and transformative ideas worldwide.",
+  },
+  {
+    title: "TEDx",
+    subtitle: "",
+    description:
+      'TEDx promotes "ideas worth spreading" through independently organized events worldwide. Driven by passionate individuals, these events bring fresh ideas and stories to inspire and spark meaningful conversations.',
+    subdescription:
+      "Guided by TED’s principles, TEDx events follow specific guidelines and host over 3,000 events annually, making a global impact at the grassroots level.",
+  },
+];
