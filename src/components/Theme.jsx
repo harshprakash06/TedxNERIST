5ade01580125fef43340c0f88b3fd5ef4413e9fb
Harsh <harshprakash06@outlook.com>
2025-03-22 23:49:15 +0530

    updated theme

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../css/Theme.css";
 const Theme = () => {
   return (
     <div className="flex flex-col md:flex-row items-start justify-center p-8 text-white mx-0 relative w-full">
@@ -14,9 +14,10 @@ const Theme = () => {
       </div>
 
       {/* Right Side - Text (Full Width, No Margin) */}
-      <div className="w-full text-left px-0 md:ml-[20%] mt-[1%] flex flex-col gap-12">
-        <p className="mt-3 text-2xl md:text-4xl mr-0 theme-text justify-around md:leading-14">
-          ❝<span className="italic ">Lighthouse Apus</span> embodies a radiant
+      <div className="w-full text-left px-0 md:ml-[20%] mt-[1%] flex flex-col gap-12 pl-5">
+        <p className="mt-3 theme-text mr-0 md:leading-14">
+          <span className="theme-text-quotaion"></span>
+          <span className="italic ">Lighthouse Apus</span> embodies a radiant
           beacon, illuminating pathways toward imaginative discovery and guiding
           explorers through uncharted realms with clarity and purpose. Inspired
           by the awe-inspiring constellation Apus—an elegant, vibrant
@@ -29,9 +30,9 @@ const Theme = () => {
           forward. Ultimately, Lighthouse Apus shines a dazzling light upon
           daring innovation, fostering collaborative creativity, and uniting
           unique abilities in an inspired journey toward boundless
-          possibilities.❞
+          possibilities.
         </p>
-        <div className="h-[400px] w-full bg-gray-400 rounded-4xl"></div>
+        <div className="h-[200px] w-full bg-gray-400 rounded-4xl"></div>
       </div>
     </div>
   );
