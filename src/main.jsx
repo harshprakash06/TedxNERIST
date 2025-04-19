c0b8e3500d410c90ac61037661b461464a8325e5
Harsh <harshprakash06@outlook.com>
2025-04-19 13:29:33 +0530

    miau

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { Analytics } from "@vercel/analytics/react";
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
@@ -9,5 +9,6 @@ if ("serviceWorker" in navigator) {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
